27d98017f0454613788781e692efc6506efa924c
Julien V <vier.julien@googlemail.com>
2022-02-06 16:30:44 +0000

    Uploaded all files for my portfolio

---
+++ main.js
@@ -0,0 +1,27 @@
+function scrollFunctionWork() {
+    let e = document.getElementById("work-anchor");
+    e.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth',
+      inline: 'start'
+    });
+  }
+
+function scrollFunctionProjects() {
+    let e = document.getElementById("projects-anchor");
+    e.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth',
+      inline: 'start'
+    });
+}
+
+
+function scrollFunctionAbout() {
+    let e = document.getElementById("about-anchor");
+    e.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth',
+      inline: 'start'
+    });
+}
